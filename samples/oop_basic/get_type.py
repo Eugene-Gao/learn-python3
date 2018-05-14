2e4374144f86670bf73e5c8239b263f84a6f0cd1
Eugene-Gao <30361789+Eugene-Gao@users.noreply.github.com>
2018-05-14 22:04:04 +0800

    Update get_type.py
    show how to use "types" module

--- samples/oop_basic/get_type.py
+++ samples/oop_basic/get_type.py
@@ -10,5 +10,12 @@ print('type(abs) =', type(abs))
 
 import types
 
-print('type(\'abc\')==str?', type('abc')==str)
+def fn():
+    pass
+
+
+print(type(fn) == types.FunctionType)
+print(type(abs) == types.BuiltinFunctionType)
+print(type(lambda x: x) == types.LambdaType)
+print(type((x for x in range(10))) == types.GeneratorType)
 
