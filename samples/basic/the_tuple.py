782c9d79c91e6221fae2ab3af2902b5727569f62
Qian Gao <gao.731@163.com>
2019-07-25 09:22:03 +0800

    First commit!

--- samples/basic/the_tuple.py
+++ samples/basic/the_tuple.py
@@ -11,3 +11,15 @@ print('classmates[-1] =', classmates[-1])
 
 # cannot modify tuple:
 classmates[0] = 'Adam'
+# -----------------------------------------------
+L = [
+    ['Apple', 'Google', 'Microsoft'],
+    ['Java', 'Python', 'Ruby', 'PHP'],
+    ['Adam', 'Bart', 'Lisa']
+]
+# 打印 Apple:
+print(L[0][0])
+# 打印 Python:
+print(L[1][1])
+# 打印 Lisa:
+print(L[2][2])
